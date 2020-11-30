b7f45db6f17cb344e662c1c23bb33b944b7e4452
your_username <kajantek@gmail.com>
2020-11-30 22:58:25 +0100

    components moved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
 
-import './App.css';
 
 class App extends Component {
  constructor() {
@@ -21,11 +21,7 @@ componentDidMount(){
   render() {
    return (
     <div className="App">
-      <header className="App-header">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
-      </header>
+      
     </div>
    );
  }
